336479f544f98174bb1a59cbc768dded49ceafd3
John <johnlouisstephenson@gmail.com>
2025-04-18 17:58:56 +0100

    refactor: clean __root

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { TankListPage } from "@/features/tanks/pages/TankListPage";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+    <>
+      <TankListPage />
+    </>
+  );
 }
