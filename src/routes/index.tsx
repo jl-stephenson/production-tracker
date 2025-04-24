fd68513c0cf02058fe1bf671b23605d8eedc415f
John <johnlouisstephenson@gmail.com>
2025-04-24 13:10:24 +0100

    refactor: global styling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,46 @@ export const Route = createFileRoute("/")({
 function Index() {
   return (
     <>
-      <Link to="/tanks">Tanks</Link>
+      <main className="dashboard-container">
+        <div className="dashboard-grid">
+          <div className="dashboard-card">
+            <div className="dashboard-card-header">
+              <h2>Tanks</h2>
+            </div>
+            <div className="dashboard-card-body">
+              <p>Something here.</p>
+              <p>And another thing.</p>
+            </div>
+            <div className="dashboard-card-footer">
+              <Link className="dashboard-card-link" to="/tanks">View</Link>
+            </div>
+          </div>
+          <div className="dashboard-card">
+            <div className="dashboard-card-header">
+              <h2>Analytics</h2>
+            </div>
+            <div className="dashboard-card-body">
+              <p>Something here.</p>
+              <p>And another thing.</p>
+            </div>
+            <div className="dashboard-card-footer">
+              <Link aria-disabled={true} className="dashboard-card-link" to="/tanks" onClick={(event) => event.preventDefault()}>View</Link>
+            </div>
+          </div>
+          <div className="dashboard-card">
+            <div className="dashboard-card-header">
+              <h2>Upcoming Tasks</h2>
+            </div>
+            <div className="dashboard-card-body">
+              <p>Something here.</p>
+              <p>And another thing.</p>
+            </div>
+            <div className="dashboard-card-footer">
+            <Link aria-disabled={true} className="dashboard-card-link" to="/tanks" onClick={(event) => event.preventDefault()}>Edit</Link>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
